fb79707a9b79d6c62271b4db2b8fd8ffea09d03c
d00055a <danieldedja2@gmail.com>
2025-04-18 19:46:09 +0200

    Add files via upload

---
+++ forgot_password_process.php
@@ -0,0 +1,66 @@
+
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/autoload.php'; 
+
+include 'config.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+
+    $sql = "SELECT * FROM perdorues WHERE email = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows > 0) {
+        $kodi_reset = bin2hex(random_bytes(32));
+
+        $sqlInsert = "INSERT INTO rikuperim_fjalekalimi (email_perdoruesi, kodi_reset) VALUES (?, ?)";
+        $stmtInsert = $conn->prepare($sqlInsert);
+        $stmtInsert->bind_param("ss", $email, $kodi_reset);
+        $stmtInsert->execute();
+
+        $link = "http://localhost/reset_password_form.php?code=$kodi_reset";
+
+        $mail = new PHPMailer(true);
+
+        try {
+
+            $mail->isSMTP();
+            $mail->Host = 'smtp.gmail.com';
+            $mail->SMTPAuth = true;
+            $mail->Username = 'danieldedja2@gmail.com';  
+            $mail->Password = 'jflydrhgyhabylcn';           
+            $mail->SMTPSecure = 'tls';                 
+            $mail->Port = 587;
+
+            $mail->setFrom('danieldedja2@gmail.com', 'FitTech');
+            $mail->addAddress($email);
+
+            $mail->isHTML(true);
+            $mail->Subject = 'Rivendos fjalekalimin - FitTech';
+            $mail->Body = "Per te ndryshuar fjalekalimin, kliko linkun me poshte:<br><a href='$link'>$link</a>";
+
+            $mail->send();
+            echo "Email-i u dergua me sukses. Kontrollo email-in per te rivendosur fjalekalimin.";
+        } catch (Exception $e) {
+            echo "Gabim gjate dergimit te emailit: {$mail->ErrorInfo}";
+        }
+
+    } else {
+        echo "Emaili nuk ekziston ne sistem!";
+    }
+
+    $stmt->close();
+    $conn->close();
+} else {
+    http_response_code(405);
+    echo "Metode jo e lejuar.";
+}
+?>
