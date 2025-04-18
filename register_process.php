2315f0e047acdf91da8d19f9b38f59d19389148a
d00055a <danieldedja2@gmail.com>
2025-04-18 17:39:27 +0200

    Update register_process.php

--- register_process.php
+++ register_process.php
@@ -1,66 +1,71 @@
-
-<?php
+<?php 
 
 include 'config.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-$validime_array=array();
+    $validime_array = [];
 
-$emri=mysqli_real_escape_string($conn, $_POST['emer']);
-$mbiemri=mysqli_real_escape_string($conn, $_POST['mbiemer']);
-$email=mysqli_real_escape_string($conn, $_POST['email']);
-$password=mysqli_real_escape_string($conn, $_POST['password']);
-$roli=$_POST['roli'];
+    $emri = mysqli_real_escape_string($conn, $_POST['emer']);
+    $mbiemri = mysqli_real_escape_string($conn, $_POST['mbiemer']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $password = mysqli_real_escape_string($conn, $_POST['password']);
+    $roli = $_POST['roli'];
 
-   if (empty($emri)){
-    $validime_array[]="Fusha per emrin duhet te plotesohet";}
-    if (empty($mbiemri)){
-    $validime_array[]="Fusha per mbiemrin duhet te plotesohet";}
-    if (empty($email)){
-    $validime_array[]="Fusha per email duhet te plotesohet";}
-    if(empty($password)){
-    $validime_array[]="Fusha per fjalekalimin duhet te plotesohet";}
-    if (strlen($password) < 6 ){
-    $validime_array[]="Fjalekalimi duhet te kete te pakten 6 karaktere";}
+    if (empty($emri)) $validime_array[] = "Fusha per emrin duhet te plotesohet";
+    if (empty($mbiemri)) $validime_array[] = "Fusha per mbiemrin duhet te plotesohet";
+    if (empty($email)) $validime_array[] = "Fusha per email duhet te plotesohet";
+    if (empty($password)) $validime_array[] = "Fusha per fjalekalimin duhet te plotesohet";
+    if (strlen($password) < 6) $validime_array[] = "Fjalekalimi duhet te ketete pakten 6 karaktere.";
 
-    
-$sql1="SELECT emer FROM perdorues WHERE email= ? LIMIT 1";
-$stmt=mysqli_prepare($conn, $sql1);
-mysqli_stmt_bind_param($stmt, "s", $email);
-mysqli_stmt_execute($stmt);
-mysqli_stmt_bind_result($stmt, $emerTest);
-mysqli_stmt_fetch($stmt);
+    // Kontrollo nëse ekziston email-i
+    $sql1 = "SELECT email FROM perdorues WHERE email = ? LIMIT 1";
+    $stmt = mysqli_prepare($conn, $sql1);
+    mysqli_stmt_bind_param($stmt, "s", $email);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_store_result( $stmt);
 
-if(!empty($emerTest)){
-$validime_array[]="Ky perdorues tashme ekziston. Jeni te sigurt qe nuk jeni regjistruar?";
-}
-
-if(empty($validime_array)) {
-
-    $sql="INSERT INTO perdorues (emer, mbiemer, email, fjalekalim, krijuar_ne) VALUES (?, ?, ?, ?, NOW() )";
-    $password=password_hash($password, PASSWORD_BCRYPT);
-    $stmt=mysqli_prepare($conn, $sql);
-    mysqli_stmt_bind_param($stmt, "sssss", $emri, $mbiemri, $email, $password, $krijuar_ne);
-    if(mysqli_stmt_execute( $stmt)){
-    echo "Ju u regjistruat me sukses!";
-    } else{
-    echo "ERROR: Nuk mund te ekzekutohej query: $sql. " . mysqli_error($conn);
+    if (mysqli_stmt_num_rows($stmt) > 0) {
+        $validime_array[] = "Ky perdorues tashme ekziston. Jeni te sigurte qe nuk jeni te regjistruar?";
     }
+
+    if (empty($validime_array)) {
+        $hashed_password = password_hash($password, PASSWORD_BCRYPT);
+
+        $sql_insert_perdorues = "INSERT INTO perdorues (emer, mbiemer, email, fjalekalim, krijuar_ne) VALUES (?, ?, ?, ?, NOW())";
+        $stmt = mysqli_prepare($conn, $sql_insert_perdorues);
+        mysqli_stmt_bind_param($stmt, "ssss", $emri, $mbiemri, $email, $hashed_password);
+
+        if (mysqli_stmt_execute($stmt)) {
+            $id_perdoruesi = mysqli_insert_id($conn); 
+
+            
+            $sql_insert_roli = "INSERT INTO roli (emer_roli, id_perdoruesi) VALUES (?, ?)";
+            $stmt2 = mysqli_prepare($conn, $sql_insert_roli);
+            mysqli_stmt_bind_param($stmt2, "si", $roli, $id_perdoruesi);
+            
+            if (mysqli_stmt_execute($stmt2)) {
+                echo "Ju u regjistruat me sukses!";
+            } else {
+                echo "Gabim gjate zgjedhjes se rolit: " . mysqli_error($conn);
+            }
+
+        } else {
+            echo "Gabim gjate regjistrimit: " . mysqli_error($conn);
+        }
 
     } else {
         echo "<ul>";
-        foreach($validime_array as $value){
-        echo "<li style='color:red'>". $value . "</li>";
+        foreach ($validime_array as $value) {
+            echo "<li style='color:red'>" . $value . "</li>";
         }
         echo "</ul>";
-        }
+    }
 
-}   else {
+} else {
     http_response_code(405);
-    echo "Nuk mund te perdoret kjo metode";
+    echo "Metode jo e lejuar.";
 }
 
 mysqli_close($conn);
-
 ?>
