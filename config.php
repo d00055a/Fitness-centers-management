fc8787047f72af59617e16628ae433c40fc7ee1c
d00055a <danieldedja2@gmail.com>
2025-04-22 00:29:47 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,21 @@
+
+<?php
+
+$emriServer="localhost";
+$username="root";
+$password="";
+$db="qender_fitnesi_db";
+
+$conn=mysqli_connect( $emriServer, $username, $password, $db);
+
+if(!$conn){
+
+echo "Problem me lidhjen ".mysqli_connect_errno();
+
+exit;
+
+}
+
+
+
+?>
